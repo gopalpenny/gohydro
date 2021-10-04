001e0fddcd58481892f89466cf476fa99f4b47de
Gopal Penny <gopalpenny@gmail.com>
2021-10-04 17:51:53 -0400

    changed interpolate_IDS to output only vector of interpolated values

--- R/interpolation.R
+++ R/interpolation.R
@@ -32,21 +32,34 @@
 #'   geom_raster(data = out_xy, aes(x, y, fill = elev)) + coord_equal()
 #'
 #' # Interpolate precipitation
-#' precip_interp <- interpolate_IDS(precip_df, out_xy, elev_gradient = 0)
-#' precip_interp <- interpolate_IDS(precip_df, out_xy, elev_gradient = 0.1)
+#' out_xy$precip_interp1 <- interpolate_IDS(precip_df, out_xy, elev_gradient = 0)
+#' out_xy$precip_interp2 <- interpolate_IDS(precip_df, out_xy, elev_gradient = 0.1)
 #'
 #' # Plot the data
 #' ggplot() +
-#'   geom_raster(data = precip_interp, aes(x, y, fill = precip)) +
+#'   geom_raster(data = out_xy, aes(x, y, fill = precip_interp2)) +
 #'   geom_point(data = precip_df, aes(x, y, fill = precip), shape = 21, size = 3) +
 #'   scale_fill_viridis_c() +
 #'   coord_equal()
 interpolate_IDS <- function(precip_df, out_xy, elev_gradient = 0) {
+  # if elev_gradient == 0, can safely set elev to 0 (covers situations when elev not provided)
+  if (elev_gradient==0) {
+    precip_df$elev <- 0
+    out_xy$elev <- 0
+  } else {
+    if (!("elev" %in% names(precip_df))) {
+      stop("precip_df must contain a column elev if elev_gradient != 0")
+    }
+    if (!("elev" %in% names(out_xy))) {
+      stop("out_xy must contain a column elev if elev_gradient != 0")
+    }
+  }
+
   # If elev_gradient != 0, convert to common elevation (mean(elev))
   elev_common <- mean(precip_df$elev)
   precip_prep <- precip_df %>%
     dplyr::mutate(precip_common_elev = precip + (elev_common - elev) * elev_gradient) %>%
-    tidyr::crossing(out_xy %>% dplyr::rename(x_out = x, y_out = y, elev_out = elev))
+    tidyr::crossing(out_xy %>% dplyr::select(x_out = x, y_out = y, elev_out = elev))
   precip_interp <- precip_prep %>%
     dplyr::mutate(dist2 = (x_out - x)^2 + (y_out - y)^2,
                   inv_dist2 = 1/dist2)  %>%
@@ -54,5 +67,12 @@ interpolate_IDS <- function(precip_df, out_xy, elev_gradient = 0) {
     dplyr::summarize(precip_common_elev_interp = weighted.mean(precip_common_elev, inv_dist2)) %>%
     dplyr::mutate(precip_interp = precip_common_elev_interp + (elev_out - precip_common_elev_interp) * elev_gradient)
 
-  return(precip_interp %>% dplyr::select(x = x_out, y = y_out, precip = precip_interp))
+  out_precip <- out_xy %>% dplyr::select("x","y") %>%
+    dplyr::left_join(precip_interp %>% dplyr::select(x = x_out, y = y_out, precip = precip_interp), by = c("x","y"))
+
+  if(nrow(out_precip) != nrow(out_xy)) {
+    stop("error -- number of rows in output is not equal to number of rows in input. Are some x-y combinations doubled?")
+  }
+
+  return(out_precip$precip)
 }
